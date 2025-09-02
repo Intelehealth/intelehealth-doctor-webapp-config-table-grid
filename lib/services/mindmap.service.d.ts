3817892e53cab52a59814998ead46bfb95c1b4c0
shaheen-p <spathan@intelehealth>
2025-09-02 13:49:19 +0530

    feat: updated test ids and rendrHtml function

--- lib/services/mindmap.service.d.ts
+++ lib/services/mindmap.service.d.ts
@@ -62,9 +62,9 @@ export declare class MindmapService {
     */
     notifyHwForRescheduleAppointment(appointment: any): void;
     /**
-    * Send cancel notification to health worker
-    * @returns {void}
-    */
+   * Send cancel notification to health worker
+   * @returns {void}
+   */
     notifyHwForCancelAppointment(appointment: any): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<MindmapService, never>;
     static ɵprov: i0.ɵɵInjectableDeclaration<MindmapService>;
