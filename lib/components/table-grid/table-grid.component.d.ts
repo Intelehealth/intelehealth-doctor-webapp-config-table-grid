5dca9aca18f36c50a966f05c3c14d6a5c9527968
dhaval-intelehealth <dhaval@intelehealth.org>
2025-10-27 13:30:55 +0530

    fix: added for appointment

--- lib/components/table-grid/table-grid.component.d.ts
+++ lib/components/table-grid/table-grid.component.d.ts
@@ -359,6 +359,7 @@ export declare class TableGridComponent implements OnInit, AfterViewInit {
      */
     emitVisitsCount(visitsCount: number): void;
     getData(event?: PageEvent): PageEvent;
+    getAppointmentData(event: PageEvent): PageEvent;
     /**
      * Fetch more data from API based on current plugin type
      */
