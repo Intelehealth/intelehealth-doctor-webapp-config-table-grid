6ac6e05ba1a33ad524a2fabbb2d431b7ae6e068e
shaheen-p <spathan@intelehealth>
2026-09-04 19:12:12 +0530

    feat: fixed follow up count issue on dashboard

--- fesm2022/ih-library.mjs
+++ fesm2022/ih-library.mjs
@@ -2220,12 +2220,12 @@ class TableGridComponent {
         this.visitService.getFollowUpVisits(this.specialization, page).subscribe({
             next: (res) => {
                 if (res.success) {
-                    this.totalRecords = res.totalCount;
                     this.recordsFetched += this.pageSize;
-                    this.emitVisitsCount(this.totalRecords);
                     const processedVisits = res.data
                         .map(visit => this.processFollowUpVisitData(visit))
                         .filter(visit => visit !== null);
+                    this.totalRecords = (page === 1 ? 0 : this.totalRecords) + processedVisits.length;
+                    this.emitVisitsCount(this.totalRecords);
                     this.followUpVisits.push(...processedVisits);
                     this.updateDataSources(this.followUpVisits, processedVisits);
                 }
